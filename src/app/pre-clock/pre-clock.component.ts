acce44d02caf97b1576c68b3504a6336a3fb27a5
Gal Levi <Galevi33@gmail.com>
2022-12-14 23:17:51 +0200

    4

--- src/app/pre-clock/pre-clock.component.ts
+++ src/app/pre-clock/pre-clock.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-pre-clock',
   templateUrl: './pre-clock.component.html',
-  styleUrls: ['./pre-clock.component.css']
+  styleUrls: ['./pre-clock.component.css'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ]
 })
 export class PreClockComponent implements OnInit {
+  isOpen = true;
 
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
-
+ 
 }
